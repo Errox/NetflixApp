dddcfd1ddb685a499cb4bb24abe0961e2648a734
Axon Robotics <sjoerdteunisse@gmail.com>
2018-09-12 15:15:43 +0200

    Changed DAO SQL implementations

--- src/DataStorageLayer/SqlServer/SqlServerEpisodeDAO.java
+++ src/DataStorageLayer/SqlServer/SqlServerEpisodeDAO.java
@@ -1,13 +1,23 @@
 package DataStorageLayer.SqlServer;
 
 import DataStorageLayer.DAO.EpisodeDAO;
+import DataStorageLayer.Helpers.MSSQLHelper;
 import DomainModelLayer.Episode;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class SqlServerEpisodeDAO implements EpisodeDAO {
+
+    private MSSQLHelper MSSQLDatabase;
+
+    public SqlServerEpisodeDAO() {
+        this.MSSQLDatabase = new MSSQLHelper();
+    }
 
     @Override
     public List<Episode> getAllEpisodes() {
@@ -30,7 +40,38 @@ public class SqlServerEpisodeDAO implements EpisodeDAO {
 
     @Override
     public Episode getEpisodeById(int id) {
-        return null;
+        Connection connection =  MSSQLDatabase.getConnection();
+        Episode episode = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try{
+            String sqlQuery = "";
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(sqlQuery);
+
+            while(resultSet.next()){
+//
+//                int subscriptionId = resultSet.getInt("subscriptionId");
+//                String name = resultSet.getString("name");
+//                String streetName = resultSet.getString("streetName");
+//                String postalCode = resultSet.getString("postalCode");
+//                String houseNumber = resultSet.getString("houseNumber");
+//                String place = resultSet.getString("place");
+
+                //Add our account from db to list.
+                episode =null;//new Episode(subscriptionId, name, streetName, postalCode, houseNumber, place);
+            }
+
+        }catch (Exception e){
+            //Print on error.
+            e.printStackTrace();
+        }finally {
+            //Clean our resources.
+            MSSQLDatabase.closeResources(resultSet, statement, connection);
+        }
+
+        return episode;
     }
 
 }
--- src/DataStorageLayer/SqlServer/SqlServerProfileDAO.java
+++ src/DataStorageLayer/SqlServer/SqlServerProfileDAO.java
@@ -1,11 +1,15 @@
 package DataStorageLayer.SqlServer;
 
 import DataStorageLayer.DAO.ProfileDAO;
+import DataStorageLayer.Helpers.MSSQLHelper;
 import DomainModelLayer.Profile;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
 public class SqlServerProfileDAO implements ProfileDAO {
 
@@ -13,20 +17,43 @@ public class SqlServerProfileDAO implements ProfileDAO {
     //[Name] [nvarchar](50) NOT NULL,
     //[Birthdate] [date] NULL,
 
+    private MSSQLHelper MSSQLDatabase;
+
+    public SqlServerProfileDAO() {
+        this.MSSQLDatabase = new MSSQLHelper();
+    }
+
     @Override
     public List<Profile> getAllProfiles() {
+        Connection connection =  MSSQLDatabase.getConnection();
         List<Profile> profiles = new ArrayList<Profile>();
-        List<Map<String, Object>> queryResult = null; //SqlHelperResultSet.getInstance().execRawQuery("SELECT * FROM Profile");
+        Statement statement = null;
+        ResultSet resultSet = null;
 
-        for (int i = 0; i < queryResult.size(); i++){
-            //Store separately to Map to get values.
-            Map<String, Object> objectMap = queryResult.get(i);
+        try{
+            String sqlQuery = "";
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(sqlQuery);
 
-            //Case Sensitively retrieving by column name.
-            //Should be by reflection.
-            Profile profile = new Profile("", new Date());
+            while(resultSet.next()){
 
-            profiles.add(profile);
+                int subscriptionId = resultSet.getInt("subscriptionId");
+                String name = resultSet.getString("name");
+                String streetName = resultSet.getString("streetName");
+                String postalCode = resultSet.getString("postalCode");
+                String houseNumber = resultSet.getString("houseNumber");
+                String place = resultSet.getString("place");
+
+                //Add our account from resultSet to list.
+                profiles.add(null);//new Profile(subscriptionId, name, streetName, postalCode, houseNumber, place));
+            }
+
+        }catch (Exception e){
+            //Print on error.
+            e.printStackTrace();
+        }finally {
+            //Clean our resources.
+            MSSQLDatabase.closeResources(resultSet, statement, connection);
         }
 
         return profiles;
@@ -34,22 +61,114 @@ public class SqlServerProfileDAO implements ProfileDAO {
 
     @Override
     public Profile getProfileById(int id) {
-        return null;
+        Connection connection =  MSSQLDatabase.getConnection();
+        Profile profile = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try{
+            String sqlQuery = "";
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(sqlQuery);
+
+            while(resultSet.next()){
+
+//                int subscriptionId = resultSet.getInt("subscriptionId");
+//                String name = resultSet.getString("name");
+//                String streetName = resultSet.getString("streetName");
+//                String postalCode = resultSet.getString("postalCode");
+//                String houseNumber = resultSet.getString("houseNumber");
+//                String place = resultSet.getString("place");
+
+                //Add our account from db to list.
+                profile = null;//new Account(subscriptionId, name, streetName, postalCode, houseNumber, place);
+            }
+
+        }catch (Exception e){
+            //Print on error.
+            e.printStackTrace();
+        }finally {
+            //Clean our resources.
+            MSSQLDatabase.closeResources(resultSet, statement, connection);
+        }
+
+        return profile;
     }
 
     @Override
     public void addProfiles(Profile newProfiles) {
+        Connection connection =  MSSQLDatabase.getConnection();
+        PreparedStatement preparedStatement = null;
 
+        //Finalize query
+        try{
+            String sqlQuery = "INSERT INTO Profiles VALUES ( ?, ?, ?, ?, ? )";
+            preparedStatement = connection.prepareStatement(sqlQuery);
+
+            //Index 1 or 0?
+//            preparedStatement.setString(1, newAccount.getName());
+//            preparedStatement.setString(2, newAccount.getStreetName());
+//            preparedStatement.setString(3, newAccount.getPostalCode());
+//            preparedStatement.setString(4, newAccount.getHouseNumber());
+//            preparedStatement.setString(5, newAccount.getPlace());
+
+        }catch (Exception e){
+            //Print on error.
+            e.printStackTrace();
+        }finally {
+            //Clean our resources.
+            MSSQLDatabase.closeStatementResources(preparedStatement);
+            MSSQLDatabase.closeConnectionResource(connection);
+        }
     }
 
 
     @Override
     public void updateProfile(Profile oldProfile, Profile newProfile) {
+        Connection connection =  MSSQLDatabase.getConnection();
+        PreparedStatement preparedStatement = null;
 
+        //Finalize query
+        try{
+            String sqlQuery = "UPDATE Accounts SET name = ?, streetName = ?, postalCode = ?, houseNumber = ?, place = ? WHERE subscriptionId = ?";
+            preparedStatement = connection.prepareStatement(sqlQuery);
+
+            //Index 1 or 0?
+//            preparedStatement.setString(1, oldAccount.getName());
+//            preparedStatement.setString(2, oldAccount.getStreetName());
+//            preparedStatement.setString(3, oldAccount.getPostalCode());
+//            preparedStatement.setString(4, oldAccount.getHouseNumber());
+//            preparedStatement.setString(5, oldAccount.getPlace());
+//            preparedStatement.setInt(6, oldAccount.getSubscriptionNumber());
+
+        }catch (Exception e){
+            //Print on error.
+            e.printStackTrace();
+        }finally {
+            //Clean our resources.
+            MSSQLDatabase.closeStatementResources(preparedStatement);
+            MSSQLDatabase.closeConnectionResource(connection);
+
+        }
     }
 
     @Override
     public void deleteProfiles(Profile deleteProfile) {
+        Connection connection =  MSSQLDatabase.getConnection();
+        Statement statement = null;
 
+        //Finalize with parameter query
+        try{
+            String sqlQuery = "DELETE FROM Accounts WHERE subscriptionId " + deleteProfile.getSubscriptionNumber();
+            statement = connection.createStatement();
+            statement.execute(sqlQuery);
+        }catch (Exception e){
+            //Print on error.
+            e.printStackTrace();
+        }finally {
+            //Clean our resources.
+            MSSQLDatabase.closeStatementResources(statement);
+            MSSQLDatabase.closeConnectionResource(connection);
+        }
     }
 }
