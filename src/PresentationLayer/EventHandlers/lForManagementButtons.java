bb2c2f60fa8b55b5c1401ead6e95b52dd388f80c
Axon Robotics <sjoerdteunisse@gmail.com>
2018-09-12 18:12:41 +0200

    Added a listner + added a part of managing
    +added controls
    +added listner

---
+++ src/PresentationLayer/EventHandlers/lForManagementButtons.java
@@ -0,0 +1,35 @@
+package PresentationLayer.EventHandlers;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class lForManagementButtons implements ActionListener {
+
+    private JPanel managementPanel;
+    private JPanel updatePanel;
+
+    public lForManagementButtons(JPanel managementPanel, JPanel updatePanel){
+            this.managementPanel = managementPanel;
+            this.updatePanel = updatePanel;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JButton button = (JButton)e.getSource();
+
+        if(button.getText() == "Create"){
+            managementPanel.setVisible(false);
+            updatePanel.setVisible(true);
+        }
+        else if(button.getText() == "Update"){
+
+        }
+        else if(button.getText() == "Delete"){
+            int dialogResult = JOptionPane.showConfirmDialog (null, "Are you sure you want to delete the selected record?","Warning",JOptionPane.YES_NO_OPTION);
+            if(dialogResult == JOptionPane.YES_OPTION){
+                // Saving code here
+            }
+        }
+    }
+}
--- src/PresentationLayer/ManagePanel.java
+++ src/PresentationLayer/ManagePanel.java
@@ -1,5 +1,7 @@
 package PresentationLayer;
 
+import PresentationLayer.EventHandlers.lForManagementButtons;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -12,6 +14,7 @@ public class ManagePanel extends JPanel {
     }
 
     private ManageType manageType;
+    private JPanel updatePanel;
 
     public ManagePanel(ManageType manageType){
         this.manageType = manageType;
@@ -19,12 +22,92 @@ public class ManagePanel extends JPanel {
         //JPanel related.
         setLayout(new BorderLayout());
 
-
-//        updatePanel = createUpdatePanel();
-//        add(createButtonPanel(), BorderLayout.SOUTH);
-//        add(createContentPanel(), BorderLayout.NORTH);
-//        add(updatePanel, BorderLayout.CENTER);
+        updatePanel = createUpdatePanel();
+        add(createButtonPanel(), BorderLayout.SOUTH);
+        add(createContentPanel(), BorderLayout.NORTH);
+        add(updatePanel, BorderLayout.CENTER);
     }
 
+    private JPanel createContentPanel() {
+        JPanel panel = new JPanel(new GridLayout(3, 1));
+
+
+        JLabel manageLabel = new JLabel("Managing " + manageType.toString().toLowerCase());
+        panel.add(manageLabel);
+
+        JLabel underManage = new JLabel("test");
+        panel.add(underManage);
+
+        JComboBox comboBox = new JComboBox<>();
+        panel.add(comboBox);
+
+        return panel;
+    }
+
+    private JPanel createButtonPanel() {
+        JPanel panel = new JPanel(new GridLayout(1, 3));
+
+        JButton createButton = new JButton("Create");
+        panel.add(createButton);
+
+        JButton updateButton = new JButton("Update");
+        panel.add(updateButton);
+
+        JButton deleteButton = new JButton("Delete");
+        panel.add(deleteButton);
+
+        lForManagementButtons managementButtonsEvents =new lForManagementButtons(panel, updatePanel);
+        createButton.addActionListener(managementButtonsEvents);
+        updateButton.addActionListener(managementButtonsEvents);
+        deleteButton.addActionListener(managementButtonsEvents);
+
+        return panel;
+    }
+
+    private JPanel createUpdatePanel() {
+        JPanel panel = new JPanel(new GridLayout(0, 2));
+        panel.setVisible(false);
+        panel.setPreferredSize(new Dimension(100, 100));
+        //setDefaultStyle(panel);
+
+        JLabel a = new JLabel("name");
+        panel.add(a);
+
+
+        JTextArea b = new JTextArea();
+        panel.add(b);
+
+        JLabel a1 = new JLabel("streetName");
+        a1.setVerticalAlignment(0);
+        panel.add(a1);
+
+        JTextArea b1 = new JTextArea();
+        panel.add(b1);
+
+        JLabel a2 = new JLabel("postalCode");
+        panel.add(a2);
+
+        JTextArea b2 = new JTextArea();
+        panel.add(b2);
+
+        JLabel a3 = new JLabel("houseNumber");
+        panel.add(a3);
+
+        JTextArea b3 = new JTextArea();
+        panel.add(b3);
+
+        JLabel a4 = new JLabel("place");
+        panel.add(a4);
+
+        JTextArea b4 = new JTextArea();
+        panel.add(b4);
+
+        JButton saveButton = new JButton("Save");
+        JButton discardButton = new JButton("Discard");
+
+        panel.add(saveButton);
+        panel.add(discardButton);
+        return panel;
+    }
 
 }
