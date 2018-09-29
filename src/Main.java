84264fe3e87a317a78f2a6660c252c29620fc677
AXR <sjoerdteunisse@users.noreply.github.com>
2018-09-29 19:04:21 +0200

    Added Comments

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,9 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+        //Initialize the splashscreen.
         new SplashScreen().initialize();
+        //Call invokelater to start a new Instance of MainForm
         SwingUtilities.invokeLater(new MainForm());
     }
 }
