80c2b9303bef2d7e5c3fbb983fdb63da36c4e882
Axon Robotics <sjoerdteunisse@gmail.com>
2018-09-29 18:16:56 +0200

    Cleaned AccountPanel Code.

--- src/PresentationLayer/Panels/AccountPanel.java
+++ src/PresentationLayer/Panels/AccountPanel.java
@@ -12,32 +12,31 @@ import java.util.List;
 
 public class AccountPanel extends JPanel {
 
-    private AccountManager accountManager = new AccountManager();
-
-    //Done, Fine tune code. remove the double list, and priv variable.
-
     public AccountPanel() {
+        // To build model for JList
         DefaultListModel listModel = new DefaultListModel();
 
+        AccountManager accountManager = new AccountManager();
         ProfileManager profileManager = new ProfileManager();
 
+        //List all accounts.
         List<Account> accountList = new ArrayList<>();
         accountList.addAll(accountManager.getAllAccounts());
 
         List<Account> accountsWithOneProfile = new ArrayList<>();
 
         for(int i = 0; i < accountList.size(); i++){
+            //As specified. List accounts with only one profile.
             if(profileManager.getProfileCount(accountList.get(i)) == 1)
                 accountsWithOneProfile.add(accountList.get(i));
         }
 
+        //Loop through and add all.
         for(int i = 0; i < accountsWithOneProfile.size(); i++) {
             listModel.addElement(accountsWithOneProfile.get(i).toString());
         }
 
         JList listSingularAccounts = new JList(listModel);
-
         add(listSingularAccounts);
-
     }
 }
