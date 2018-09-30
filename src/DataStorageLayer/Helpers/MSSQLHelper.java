51954fc5f3f1e76643fc1a67a0e5b7bb84cf08eb
Unknown <denbukki@gmail.com>
2018-09-30 14:54:46 +0200

    added cascading delete on users and profiles

--- src/DataStorageLayer/Helpers/MSSQLHelper.java
+++ src/DataStorageLayer/Helpers/MSSQLHelper.java
@@ -7,7 +7,7 @@ import java.sql.Statement;
 
 
 public class MSSQLHelper {
-    private static final String connectionString = "jdbc:sqlserver://AXR;databaseName=NetFlixStats;integratedSecurity=true;";
+    private static final String connectionString = "jdbc:sqlserver://DESKTOP-947ME72\\SQLEXPRESS;databaseName=NetFlixStats;integratedSecurity=true;";
     private static Connection connection;
 
     public Connection getConnection() {
